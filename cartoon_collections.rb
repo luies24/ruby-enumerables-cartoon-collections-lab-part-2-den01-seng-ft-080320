7bf41bb2b001bad68e8f1b37ad69e478759b3c19
Luis Garcia <l.garcia24@hotmail.com>
2020-07-23 05:29:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_valid_calls(planeteer_calls)
   i = 0
   
   planeteer_calls.any? do |element|
-    while i < valid_calls.count 
+    while i < valid_calls.count do
       if element == valid_calls[i]
         return element
       end
